0a7754c9806a419bff13810e1bb385e4c26644ce
GenericProgram <gddiamondnoob@gmail.com>
2023-01-06 12:57:52 -0800

    makeing it good

--- src/Piece.java
+++ src/Piece.java
@@ -1,2 +1,24 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Piece {
+    private final int color;
+    private String imgPath;
+
+    public Piece(int color, String imgPath) {
+        this.color = color;
+        this.imgPath = imgPath;
+
+        ImageIcon img = new ImageIcon(imgPath);
+
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+    public void draw(Graphics g) {
+    //TODO: implement
+    }
+
 }
