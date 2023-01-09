4ff5a592e846618e09edfcdc435ae383f27d0e71
connertm24 <connertm24@sherwoodsd.com>
2023-01-09 12:04:17 -0800

    corrected name

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
 import javax.swing.*;
 
 public class Main {
-    public static void main(String args[]) {
-        JFrame frame = new ChessGame();
+    public static void main(String args[]) {JFrame frame = new ChessGame();
         frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         frame.pack();
-        frame.setTitle("Chess");
+        frame.setTitle("Chesscapades");
         frame.setResizable(false);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
