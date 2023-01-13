27b52becfa70f30e85c89239a9df26c7a87c5f34
connertm24 <connertm24@sherwoodsd.com>
2023-01-13 12:24:15 -0800

    Rook has (good logic)

--- src/pieces/Rook.java
+++ src/pieces/Rook.java
@@ -20,6 +20,27 @@ public class Rook extends Piece {
             return null;
         }
     }
+    public boolean isLegalMove(int x, int y, int newx, int newy, Board board){
+        int yoffset = newy - y;
+        int xoffset = newx - x;
+        if (yoffset == 0){
+            for (int i = 1; i < Math.abs(yoffset); i++){
+                if (board.getTile(Board.getLocationFromCords((int)(x + (i * Math.signum(xoffset))), y)).getPiece() != null){
+                    return false;
+                }
+            }
+            return true;
+        }else if (xoffset == 0){
+            for (int i = 1; i < Math.abs(yoffset); i++){
+                if (board.getTile(Board.getLocationFromCords(x, (int)(y + (i * Math.signum(yoffset))))).getPiece() != null){
+                    return false;
+                }
+            }
+            return true;
+        }else{
+            return false;
+        }
+    }
 
 
 
