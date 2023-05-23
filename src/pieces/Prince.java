990a15533f7d565ae8fa320f7866695c04971832
thederpcodeman <connermckeone42@gmail.com>
2023-05-23 11:53:13 -0700

    prince fix

--- src/pieces/Prince.java
+++ src/pieces/Prince.java
@@ -8,6 +8,7 @@ import javax.swing.*;
     public class Prince extends Pawn{
         public Prince(int color){
             super(color);
+            value = 2;
         }
 
         public ImageIcon getImageIcon() {
