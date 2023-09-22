e2122061d7e5b3b71747b8287d1db3feacc21825
connertm24 <connermckeone42@gmail.com>
2023-09-22 12:58:00 -0700

    Skatter Chess is now rare.

--- src/Game/ChessGame.java
+++ src/Game/ChessGame.java
@@ -114,13 +114,13 @@ public class ChessGame extends JFrame implements MouseListener, MouseMotionListe
         bStab = ((int) (Math.random() * 5.5) == 1);
         touchRule = ((int) (Math.random() * 8.5) == 1);
         tLocked = false;
-        myst = ((int) (Math.random() * 12) == 1);
+        myst = ((int) (Math.random() * 10) == 1);
         skatter = false;
         if (!myst){
-            skatter = ((int) (Math.random() * 2) == 1);
+            skatter = ((int) (Math.random() * 12) == 1);
         }
         if (myst || skatter && !touchRule){
-            touchRule = ((int) (Math.random() * 2) == 1);
+            touchRule = ((int) (Math.random() * 2.5) == 1);
         }
         chessBoard.wRadness = 3;
         chessBoard.bRadness = 3;
