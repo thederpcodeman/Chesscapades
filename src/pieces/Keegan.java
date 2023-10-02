469eb77836056794c1a798f8a2f83dfb7a8f9293
connertm24 <connermckeone42@gmail.com>
2023-10-02 12:41:53 -0700

    Keegan

---
+++ src/pieces/Keegan.java
@@ -0,0 +1,43 @@
+package pieces;
+
+import Game.Board;
+import Game.Tile;
+
+import javax.swing.*;
+
+public class Keegan extends Piece {
+    public Keegan(int color) {
+        super(color);
+        value = 3;
+        this.royal = true;
+        this.wall = true;
+        name = "Keegan";
+    }
+
+    @Override
+    public ImageIcon getImageIcon() {
+        if(color == 0) {
+            return(new ImageIcon("src/resources/bUnknown.png"));
+        } else if(color == 1) {
+            return(new ImageIcon("src/resources/wUnknown.png"));
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public boolean isLegalMove(int x, int y, int newX, int newY, Board board, boolean forReal) {
+        int dx = newX - x;
+        int dy = newY - y;
+        Tile destination = board.getTile(Board.getLocationFromCords(newX, newY));
+        if (!Moves.allClear(color, destination)){
+            return false;
+        }
+        if (destination.getPiece() != null && !(destination.getPiece() instanceof Keegan)){
+            if (destination.isLegalMove(Board.getLocationFromCords(x, y), board, false)){
+                return true;
+            }
+        }
+        return ((Math.abs(dx) <= 1) && (Math.abs(dy) <= 1));
+    }
+}
